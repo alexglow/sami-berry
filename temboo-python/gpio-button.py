f8f4cf1b7a6e66ace52bee876f7bc6dd57c3f2f5
Alex Glow <alex.glowaski@gmail.com>
2015-09-16 18:51:25 -0700

    comments in gpio test script

--- temboo-python/gpio-button.py
+++ temboo-python/gpio-button.py
@@ -4,18 +4,20 @@ import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
+# Components are plugged into these digital pins
 led = 14
 button = 15
 
+# Tell the Pi the LED is an output, and the button is an input using the built-in pullup resistor
 GPIO.setup(led, GPIO.OUT)
 GPIO.setup(button, GPIO.IN, GPIO.PUD_UP)
 
+# Turn the LED on for 2 seconds, then off again
 GPIO.output(led, 1)
-
 time.sleep(2)
-
 GPIO.output(led, 0)
 
+# Wait around to see if the button is pressed. If so, blink LED
 while True:
     if GPIO.input(button) == False:
         print("Button pressed")
@@ -23,4 +25,5 @@ while True:
         time.sleep(1)
         GPIO.output(led, 0)
 
+# Clean up your mess
 GPIO.cleanup()
